a51b49e3325a376b21194d53d277805a02fd50a0
Jack920910 <jack920910@gmail.com>
2013-01-11 14:20:45 +0800

    Navigation added Main added

---
+++ Pigeon/Navigation.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Pigeon.Msg;
+using Pigeon.Maps;
+
+namespace Pigeon
+{
+    public partial class Navigation : PhoneApplicationPage
+    {
+
+        private MessageHandler _handler;
+
+        private MapManipulator _map;
+
+        public Navigation()
+        {
+            InitializeComponent();
+
+            _handler = MessageHandler.GetMessageHandler();
+            _map = new MapManipulator(mainMap);
+
+        }
+    }
+}
