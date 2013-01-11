8434fc3da21254941dd1af9c26536ba325229f18
xiaohaoliu <275589628@qq.com>
2013-01-11 15:48:12 +0800

    modified:   Bin/Debug/MDIL/Pigeon.dll 	modified:   Bin/Debug/Pigeon.dll 	modified:   Bin/Debug/Pigeon.pdb 	modified:   Bin/Debug/Pigeon_Debug_AnyCPU.xap 	modified:   Login.xaml 	modified:   Login.xaml.cs 	modified:   MainPage.xaml.cs

--- Pigeon/Login.xaml.cs
+++ Pigeon/Login.xaml.cs
@@ -7,14 +7,27 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Pigeon.Msg;
 
 namespace Pigeon
 {
     public partial class Login : PhoneApplicationPage
     {
+        MessageHandler _handler;
         public Login()
         {
             InitializeComponent();
+        }
+       
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            _handler = MessageHandler.GetMessageHandler();
+            _handler.Login(txtPhoneNumber.Text,txtPassword.Text, "cquapp.com");
+            _handler.OnLogin += LoginSuccess;
+            btnLogin.IsEnabled = false;   
+        }
+        private void LoginSuccess(object sender, Matrix.EventArgs e)
+        {
         }
     }
 }
--- Pigeon/MainPage.xaml.cs
+++ Pigeon/MainPage.xaml.cs
@@ -34,88 +34,14 @@ namespace Pigeon
         public MainPage()
         {
             InitializeComponent();
-
-            DispatcherTimer dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(33);
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Start();
         }
 
-        void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            try
-            {
-                FrameworkDispatcher.Update();
-            }
-            catch
-            {
-            }
-
-            if (true == _soundIsPlaying)
-            {
-                if (_soundInstance.State != SoundState.Playing)
-                {
-                    _soundIsPlaying = false;
-                }
-            }
-        }
 
         MessageHandler _handler;
-
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            _handler = MessageHandler.GetMessageHandler();
-            _handler.Login("cdffh", "123456", "cquapp.com");
-            _handler.OnLogin += HandlerOnLogin;
-            _handler.OnReceiveTextMessage += HandlerOnReceiveTextMessage;
-            _handler.OnReceiveGPSMessage += HandlerOnReceiveGPSMessage;
-            _handler.OnReceiveImageMessage += HandlerOnReceiveImageMessage;
-            _handler.OnReceiveVoiceMessage += HandlerOnReceiveVoiceMessage;
-
-            btnLogin.IsEnabled = false;
-        }
-
-        void HandlerOnLogin(object sender, Matrix.EventArgs e)
-        {
-            MessageBox.Show("Login Success");
-        }
-
-        void HandlerOnReceiveTextMessage(object sender, MessagingEventArgs e)
-        {
-            string message = (string)e.Body;
-            MessageBox.Show(message);
-        }
-
-        void HandlerOnReceiveGPSMessage(object sender, MessagingEventArgs e)
-        {
-            GeoCoordinate coordinate = (GeoCoordinate)e.Body;
-            MessageBox.Show("GPS = (" + coordinate.Latitude + ", " + coordinate.Longitude + ")");
-        }
-
-        void HandlerOnReceiveImageMessage(object sender, MessagingEventArgs e)
-        {
-            BitmapImage image = (BitmapImage)e.Body;
-            imgReceived.Source = image;
-        }
-
-        void HandlerOnReceiveVoiceMessage(object sender, MessagingEventArgs e)
-        {
-            _voiceBytes = (byte[])e.Body;
-
-            Thread soundThread = new Thread(new ThreadStart(PlaySound));
-            soundThread.Start();
-        }
-
-        byte[] _voiceBytes;
-
-        void PlaySound()
-        {
-            SoundEffect sound = new SoundEffect(_voiceBytes, _mic.SampleRate, AudioChannels.Mono);
-            _soundInstance = sound.CreateInstance();
-            _soundIsPlaying = true;
-            _soundInstance.Play();
-        }
-
+            NavigationService.Navigate(new Uri("/Login.xaml", UriKind.Relative));
+        }  
         private void btnRegister_Click(object sender, RoutedEventArgs e)
         {
             _handler = MessageHandler.GetMessageHandler();
@@ -125,15 +51,11 @@ namespace Pigeon
             _handler.OnRegister += HandlerOnRegister;
             _handler.OnRegisterError += HandlerOnRegisterError;
         }
-
         void HandlerOnRegister(object sender, EventArgs e)
         {
         }
-
         void HandlerOnRegisterError(object sender, Matrix.Xmpp.Client.IqEventArgs e)
         {
         }
-
-
     }
 }
